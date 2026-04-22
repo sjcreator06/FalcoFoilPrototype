5a249f60bba8bb06eb0297a334300875a8c126ba
Samuel Joseph <73075829+sjcreator06@users.noreply.github.com>
2026-04-22 04:00:46 -0700

    Update Weight_Optimization_Falco Foil.m

--- Weight_Optimization_Falco Foil.m
+++ Weight_Optimization_Falco Foil.m
@@ -7,9 +7,9 @@
 %              © 2026 Falco Foil
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
 % Wing Parameters
 MTOW = 8600;                         % Maximum Takeoff Weight (MTOW)
+MWTOW = 11000;                       % Metamorphic Wing Takeoff Weight (MWTOW)
 rho = 0.0012;                        % slug/ft^3 (Using Atmospheric Model at Crusie Altitude)
 S = 225;                             % ft^2
 V = 418;                             % ft/s
@@ -22,50 +22,69 @@ c = 5;                               % Chord
 AR = b/c;                            % Aspect Ratio
 k = 1 / (pi*AR*e);                   % Drag Polar Factor
 
-% Effeciency Calculation
+% Effeciency Calculation for Plot Dataset
 W = 1:1:50000;                       % Weight List
 C_L = (2 .* W) / (rho * V^2 * S);    % Coefficient of Lift
 C_D = C_D_o + k * C_L.^2;            % Coefficient of Drag
 E = C_L ./ C_D;                      % Effeciency
+[maxE,index] = max(E);               % Max Effeciency
+
+% Effeciency Calculation for MTOW and MWTOW Points on Plot
+C_L_MTOW = (2 * MTOW) / (rho * V^2 * S); 
+C_D_MTOW = C_D_o + k * C_L_MTOW^2;  
+E_MTOW = C_L_MTOW / C_D_MTOW;
+
+C_L_MWTOW = (2 * MWTOW) / (rho * V^2 * S); 
+C_D_MWTOW = C_D_o + k * C_L_MWTOW^2;  
+E_MWTOW = C_L_MWTOW / C_D_MWTOW;
 
 % Effeciency Plot
 subplot(1, 3, 1)
+set(gcf, 'Color', [1 1 1])
+
 plot(W,E,LineWidth=2.0)
-title("Efficiency vs Weight")
-xlabel("Weight (lbs)")
-ylabel("Efficiency")
+title("Efficiency vs Weight",'Color','k')
+xlabel("Weight (lbs)",'Color','k')
+ylabel("Efficiency",'Color','k')
 
 hold on
-MTOWList = MTOW*ones(1,length(W));
-MTOW_Vertical = linspace(0,15,length(W));
-plot(MTOWList,MTOW_Vertical,LineWidth=2.0)
+plot(MTOW,E_MTOW, "X", 'MarkerSize', 20, LineWidth=2.0)
+plot(MWTOW,E_MWTOW,"X",'MarkerSize', 20, LineWidth=2.0)
+plot(W(index),maxE,"X",'MarkerSize', 20, LineWidth=2.0)
 
-legend("Efficiency Curve","MTOW")
+ax1 = gca; 
+ax1.XAxis(1).Color = 'k'; 
+ax1.YAxis(1).Color = 'k'; 
+legend("Efficiency Curve","MTOW","MWTOW","Maximum Effeciency")
 
 % Coefficient of Lift Plot
 subplot(1, 3, 2)
 plot(W,C_L,LineWidth=2.0)
-title("Coefficient of Lift vs Weight")
-xlabel("Weight (lbs)")
-ylabel("Coefficient of Lift")
-
+title("Coefficient of Lift vs Weight",'Color','k')
+xlabel("Weight (lbs)",'Color','k')
+ylabel("Coefficient of Lift",'Color','k')
+        
 hold on
-MTOWList = MTOW*ones(1,length(W));
-MTOW_Vertical = linspace(0,2.5,length(W));
-plot(MTOWList,MTOW_Vertical,LineWidth=2.0)
+plot(MTOW,C_L_MTOW, "X", 'MarkerSize', 20, LineWidth=2.0)
+plot(MWTOW,C_L_MWTOW,"X",'MarkerSize', 20, LineWidth=2.0)
 
-legend("CL Curve","MTOW")
+ax2 = gca; 
+ax2.XAxis(1).Color = 'k'; 
+ax2.YAxis(1).Color = 'k'; 
+legend("CL Curve","MTOW","MWTOW")
 
 % Coefficient of Drag Plot
 subplot(1, 3, 3)
 plot(W,C_D,LineWidth=2.0)
-title("Coefficient of Drag vs Weight")
-xlabel("Weight (lbs)")
-ylabel("Coefficient of Drag")
+title("Coefficient of Drag vs Weight",'Color','k')
+xlabel("Weight (lbs)",'Color','k')
+ylabel("Coefficient of Drag",'Color','k')
 
 hold on
-MTOWList = MTOW*ones(1,length(W));
-MTOW_Vertical = linspace(0,0.25,length(W));
-plot(MTOWList,MTOW_Vertical,LineWidth=2.0)
+plot(MTOW,C_D_MTOW, "X", 'MarkerSize', 20, LineWidth=2.0)
+plot(MWTOW,C_D_MWTOW,"X",'MarkerSize', 20, LineWidth=2.0)
 
-legend("CD Curve","MTOW")
+ax3 = gca; 
+ax3.XAxis(1).Color = 'k'; 
+ax3.YAxis(1).Color = 'k'; 
+legend("CD Curve","MTOW","MWTOW")
